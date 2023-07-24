d211618d4892f7347b7e6082a22d7ca7814e047e
amor4330 <amor4330@gmail.com>
2023-07-25 00:14:03 +0900

    feat: [JAVA-1] #comment 조건문/반복문 연습문제 (4_8 완료, 4_11 추가) - 2023.07.25

--- src/main/java/com/arms/minjae/ch04/Example_4_8.java
+++ src/main/java/com/arms/minjae/ch04/Example_4_8.java
@@ -3,16 +3,24 @@ package com.arms.minjae.ch04;
 public class Example_4_8 {
     public static void main(String[] args) {
 
-        int x = 0;
-        int y = 0;
+        // 2x + 4y = 10
 
+        int count = 0;
 
-        while (x <= 5) {
-            if ((2 * y) % (5 - x) == 0) {
-                y++;
-                System.out.println("x: " + x + ", y: " + y);
+        for (int x = 0; 2 * x <= 10; x++) {
+                int x2 = 2 * x;
+
+            for (int y = 0; 4 * y <= 10; y++) {
+                int y4 = 4 * y;
+
+                if (x2 + y4 == 10) {
+                    System.out.println("x: " + x + ", y: " + y);
+                    break;
+                }
+                count++;
             }
-            x++;
         }
+
+        System.out.println(count);
     }
 }
